9e5dda3d3d9294f6c5f06afceb9fe22fc55b21d1
Sandro Huber <sandrohuber89@hotmail.com>
2018-12-12 20:32:14 +0000

    Also mount git config.

--- start_development.sh
+++ start_development.sh
@@ -8,5 +8,6 @@ docker run \
   -it \
   -w /usr/src/twostepsfromcode \
   --volume ${PWD}:/usr/src/twostepsfromcode \
+  --volume ~/.gitconfig:/etc/gitconfig \
   --entrypoint /bin/zsh \
   ${CONTAINER_NAME}
