5e32a0a8ce4393f760a864806bfb28892b130310
Sandro Huber <sandrohuber89@hotmail.com>
2018-12-20 22:04:21 +0000

    Embedd small elm div into html template.

--- apps/website/assets/js/app.js
+++ apps/website/assets/js/app.js
@@ -15,3 +15,9 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+import { Elm } from "../src/Main.elm";
+
+Elm.Main.init({
+  node: document.getElementById("elm-node")
+})
