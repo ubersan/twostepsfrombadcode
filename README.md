1381d47755d4161566f6826b133fc1f79f6f35b3
Sandro Huber <sandrohuber89@hotmail.com>
2018-12-27 08:20:07 +0000

    Moved to correct elm architecture implementation.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ## twostepsfrombadcode
 
 - Run `./start_development.sh` to enter dev environment.
-- Run `./fetch_dependencies.sh` to get all required dependencies for mix and node to work properly.
+- Run `mix deps.get and npm install in apps/website/assets` to get all required dependencies for mix and node to work properly.
 - Run `mix phx.server` to compile and run the website exposing port 4000 to the host.
+
+# Production
+- Configure elm-webpack-loader with `debug: options.mode === "development"` and `optimize: true` to compile elm for production and remove all debug features.
--- apps/website/assets/src/Main.elm
+++ apps/website/assets/src/Main.elm
@@ -1,6 +1,47 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Browser
 
+import Html exposing (Html, div, text)
+import Html.Events exposing (onClick)
+
+type alias Color =
+  { r: Int
+  , g: Int
+  , b: Int
+  }
+
+type alias Model = Color
+
+type Msg =
+  MouseClicked
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ({r=100, b=100, g=100}
+  , Cmd.none
+  )
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    MouseClicked ->
+      ( model, Cmd.none )
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+view : Model -> Html Msg
+view model =
+  div [] [text "Hello from elm"]
+
+main : Program () Model Msg
 main =
-  text "Hello from elm!"
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
