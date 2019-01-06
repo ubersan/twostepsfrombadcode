927bb597b71366a56ee4467e02d9caf856dff132
Sandro Huber <sandrohuber89@hotmail.com>
2019-01-06 17:50:19 +0000

    Some cleanups.

--- apps/website/lib/website_web/controllers/cube_controller.ex
+++ apps/website/lib/website_web/controllers/cube_controller.ex
@@ -6,16 +6,18 @@ defmodule WebsiteWeb.CubeController do
     {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, body}} =
       :httpc.request(:get, {'https://raw.githubusercontent.com/sandrohuber/twostepsfrombadcode/master/apps/website/assets/static/meshes/cube.obj', []}, [], [])
 
-    str_body = to_string body
-    all_lines = String.split(str_body, "\n")
-    lines = Enum.filter(all_lines, fn line -> String.starts_with?(line, "v ") end)
-    vectors = Enum.map(lines, fn line -> extract_position_vector(line) end)
+    coordinates =
+      body
+      |> to_string
+      |> String.split("\n")
+      |> Enum.filter(fn line -> String.starts_with?(line, "v ") end)
+      |> Enum.map(fn line -> extract_position_vector(line) end)
 
-    json conn, %{"cube" => vectors}
+    json conn, %{"cube" => coordinates}
   end
 
   def extract_position_vector(line) do
-    [_v, x, y, z] = String.split(line, "  ")
+    [_v, x, y, z] = String.split(line, " ")
     %{"x" => String.to_float(x), "y" => String.to_float(y), "z" => String.to_float(z)}
   end
 end
